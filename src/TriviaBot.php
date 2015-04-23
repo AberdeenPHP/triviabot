e53ffad056ab12971a73245c0fa71707beef445a
billythekid <billy@the-kid.org>
2015-04-23 14:23:35 +0100

    this isn't working, also, why is gitignore not ignoring the files in it?

---
+++ src/TriviaBot.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: billythekid
+ * Date: 23/04/15
+ * Time: 08:41
+ */
+
+namespace BTK;
+
+use ThreadMeUp\Slack;
+
+class TriviaBot {
+
+    private $currentSet;
+    private $currentQuestion;
+    private $currentAnswer;
+    private $client;
+
+    public function __construct($params)
+    {
+        $this->currentSet = (!empty($params['currentSet'])) ? $params['currentSet'] : -1;
+        $this->currentQuestion = (!empty($params['currentQuestion'])) ? $params['currentQuestion'] : "";
+        $this->currentAnswer = "";
+    }
+
+    /**
+     * @param string $channel
+     * @param string $message
+     *
+     * @return void
+     */
+    public function sendMessageToChannel($channel, $message)
+    {
+        include "config.php";
+        $this->setClient(new Slack\Client($configs));
+        $chat = $this->client->chat($channel);
+        $chat->send($message);
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getCurrentSet()
+    {
+        return $this->currentSet;
+    }
+
+    /**
+     * @param mixed $client
+     */
+    public function setClient($client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param int $currentSet
+     */
+    public function setCurrentSet($currentSet)
+    {
+        $this->currentSet = $currentSet;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrentQuestion()
+    {
+        return $this->currentQuestion;
+    }
+
+    /**
+     * @param string $currentQuestion
+     */
+    public function setCurrentQuestion($currentQuestion)
+    {
+        $this->currentQuestion = $currentQuestion;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrentAnswer()
+    {
+        return $this->currentAnswer;
+    }
+
+    /**
+     * @param string $currentAnswer
+     */
+    public function setCurrentAnswer($currentAnswer)
+    {
+        $this->currentAnswer = $currentAnswer;
+    }
+
+
+
+
+
+}
---
+++ src/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: billythekid
+ * Date: 22/04/15
+ * Time: 14:54
+ */
+include '../vendor/autoload.php';
+include 'ourConfig.php'; // .gitignored - simply creates a $config var using the settings below
+
+use ThreadMeUp\Slack\Client;
+
+$config = (!empty($config)) ? $config : [
+    'token' => 'USER-API-TOKEN',
+    'team' => 'YOUR-TEAM',
+    'username' => 'BOT-NAME',
+    'icon' => 'ICON', // Auto detects if it's an icon_url or icon_emoji
+    'parse' => '', // __construct function in Client.php calls for the parse parameter
+];
