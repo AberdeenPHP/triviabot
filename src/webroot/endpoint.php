ea4ad70db83c2ee350e5cc4c2d657c85c307ae6e
billythekid <billy@the-kid.org>
2015-04-27 22:24:36 +0100

    i'm an idiot, no need for a full slack API integration, can run this just from an outgoing webhook. d'oh!

--- src/webroot/endpoint.php
+++ src/webroot/endpoint.php
@@ -6,23 +6,23 @@
  * Time: 14:54
  */
 namespace BTK;
-include __DIR__.'../../../../../../vendor/autoload.php';
-
-use ThreadMeUp\Slack;
-use Dotenv;
 ini_set('display_errors', 1);
+require('../TriviaBot.php');
+require('../db.php');
 
-Dotenv::load(__DIR__);
-
-$config = [
-    'token' => getenv('CLIENT_TOKEN'),
-    'team' => getenv('CLIENT_TEAM'),
-    'username' => getenv('CLIENT_USERNAME'),
-    'icon' => getenv('CLIENT_ICON'),
-    'parse' => '',
-];
-
-$slack = new Slack\Client($config);
-$params = array('client'=>$slack);
+$params = [];
 $bot = new TriviaBot($params);
-$bot->sendMessageToChannel('#trivia',"testing!");
+unset($bot);//just to get rid of teh unused var warning in phpstorm!
+
+/* Example POSTed data from channel
+token=FWfMETEsdOxzYZPjm76PhJiL
+team_id=T0001
+team_domain=example
+channel_id=C2147483705
+channel_name=test
+timestamp=1355517523.000005
+user_id=U2147483697
+user_name=Steve
+text=googlebot: What is the air-speed velocity of an unladen swallow?
+trigger_word=googlebot:
+*/
