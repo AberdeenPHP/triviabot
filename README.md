a50b67e37b52504a6b7f53761d701e0f4a7bf763
billythekid <billy@the-kid.org>
2015-04-30 17:34:26 +0100

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
 # triviabot
 Trivia bot that runs in a Slack channel.
 
-To install run `composer require btk/triviabot dev-master`  on your server.
+To install run `composer require btk/triviabot dev-master`.
 
-THIS PACKAGE IS STILL IN DEVELOPMENT - seriously, it's not even ante-pre-alpha yet.
+**THIS PACKAGE IS STILL IN DEVELOPMENT** - seriously, it's not even ante-pre-alpha yet.
 It's strongly recommended to make the root of your webserver/subdomain the folder triviabot/src/webroot as this will keep your db and config files etc away from prying eyes.
 
-You will need to set up an incoming and an outgoing webhook in slack then add teh details to config.php
+Things you'll need to set up if you want this to run:
+
+* Incoming webhook integration in slack (put these in config.php)
+* Outgoing webhook in slack (put these in config.php)
+* A way of running a script at an interval (e.g. CRON) (you want to run vendor/btk/triviabot/src/run.php as often as you can stomach)
+* DB details (put these in db.php)
+
+Your webserver should ideally use a subdomain where webroot is the folder /vendor/btk/triviabot/src/webroot/ so your outgoing webhook URL can be, e.g. http://triviabot.example.com/endpoint.php
+
+###Example CRON Settings.
+
+I use a 10 second delay between running the scripts. I've tested this with over 30,000 questions loaded in the bot and it works fine for me.
+Cron however, only uses minutes as it's minimum time increment. So I set mine up like this...
+
+
+    * * * * * php /var/www/triviabot/vendor/btk/triviabot/src/run.php  
+    * * * * * sleep 10; php /var/www/triviabot/vendor/btk/triviabot/src/run.php
+    * * * * * sleep 20; php /var/www/triviabot/vendor/btk/triviabot/src/run.php
+    * * * * * sleep 30; php /var/www/triviabot/vendor/btk/triviabot/src/run.php
+    * * * * * sleep 40; php /var/www/triviabot/vendor/btk/triviabot/src/run.php
+    * * * * * sleep 50; php /var/www/triviabot/vendor/btk/triviabot/src/run.php
+
+The minimum delay between questions/hints (the output of the run.php file) is 20 seconds so this should give more than we need.
+
+###Database setup.
+
+In the vendor/btk/triviabot/ folder there is a triviabot.sql file that you can import into your database to set up your tables for the bot.
---
+++ triviabot.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Apr 30, 2015 at 04:31 PM
+-- Server version: 5.5.43-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `triviabot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games`
+--
+
+CREATE TABLE IF NOT EXISTS `games` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `started` int(11) NOT NULL,
+  `stopping` int(11) NOT NULL,
+  `delay` int(11) NOT NULL,
+  `last_asked` int(11) NOT NULL COMMENT 'php timestamp',
+  `questions_without_reply` int(11) NOT NULL DEFAULT '0',
+  `round_month` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `players`
+--
+
+CREATE TABLE IF NOT EXISTS `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `slack_id` text NOT NULL,
+  `name` text NOT NULL,
+  `last_seen` int(11) NOT NULL DEFAULT '0',
+  `current_score` int(11) NOT NULL DEFAULT '0',
+  `high_score` int(11) NOT NULL DEFAULT '0',
+  `current_run` int(11) NOT NULL DEFAULT '0',
+  `best_run` int(11) NOT NULL DEFAULT '0',
+  `playing_month` int(11) NOT NULL DEFAULT '0',
+  UNIQUE KEY `id_2` (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `set` int(11) NOT NULL,
+  `question` text NOT NULL,
+  `answer` text NOT NULL COMMENT 'serialized array of possible answers',
+  `current_hint` int(11) DEFAULT '0' COMMENT 'the question currently in play',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=33501 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question_sets`
+--
+
+CREATE TABLE IF NOT EXISTS `question_sets` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` text NOT NULL,
+  `title` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
